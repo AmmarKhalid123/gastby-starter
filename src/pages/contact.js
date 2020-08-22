5a4ab152c24e0312c99a3789aa99adf8334704b7
Ammar Khalid <ammarkhalid237@gmail.com>
2020-08-22 23:47:50 +0500

    gatsby started

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,9 +5,9 @@ import Header from '../header';
 export default function Home() {
   return (
     <div>
-        <Header text='Hello Gatsby!' />
-        <Link to='/contact/'>Contact Us</Link>
-        <div>My First Gatsby Site!</div>
+        <Header text='My Contact' />
+        <Link to='/'>Back To Home</Link>
+        <div>email: ammarkhalid237@gmail.com</div>
     </div>
   );
 }
